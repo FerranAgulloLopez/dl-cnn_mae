f55fc50ab63735cb76e737b6539184430ea8ac73
Adrián Tormos Llorente <adrian.tormos@est.fib.upc.edu>
2021-03-15 18:34:50 +0100

    Adapts MAMe data loader to code

--- code/data/types/mame.py
+++ code/data/types/mame.py
@@ -1,8 +1,10 @@
 import os
-from PIL import Image
+
+import numpy as np
 import pandas as pd
-from torchvision import transforms
+from PIL import Image
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
 from data.data import Data
 
@@ -13,14 +15,15 @@ class MAMeDataset(Dataset):
         self.split = split
         self.transform = transform
         self.metadata = pd.read_csv(metadata_path)
-        self.metadata = self.metadata[self.metadata['Subset'] == self.split]
+        self.metadata = self.metadata[self.metadata['Subset'] == self.split].reset_index(drop=True)
         self.metadata = self.metadata[['Image file', 'Medium']]
         self.metadata['Medium'] = self.metadata.apply(lambda x: key[x['Medium']], axis=1)
 
     def __getitem__(self, item):
         image = Image.open(os.path.join(self.root_dir, self.metadata.loc[item, 'Image file'])).convert('RGB')
-        label = self.metadata.loc[item, 'Medium']
-        return self.transform(image) if self.transform is not None else image, label
+        label_vector = np.zeros((29,)).astype('float32')
+        label_vector[self.metadata.loc[item, 'Medium']] = 1
+        return self.transform(image) if self.transform is not None else image, label_vector
 
     def __len__(self):
         return len(self.metadata.loc[:, 'Image file'])
@@ -29,31 +32,43 @@ class MAMeDataset(Dataset):
 class MAMe(Data):
     def __init__(self, config, device):
         super().__init__(config, device)
-        self.directory = config['directory']
+        self.metadata_directory = config['metadata_directory']
+        self.images_directory = config['images_directory']
         self.batch_size = config['batch_size']
+        if config['version'] == 'full':
+            self.metadata_file = 'MAMe_dataset.csv'
+        elif config['version'] == 'toy':
+            self.metadata_file = 'MAMe_toy_dataset.csv'
+        else:
+            raise NotImplementedError('Dataset version not implemented')
         self.label_descriptions = {
             row['description']: index
-            for index, (_, row) in enumerate(pd.read_csv(os.path.join(self.directory, 'MAMe_labels.csv'), names=["description"], index_col=0).iterrows())
+            for index, (_, row) in enumerate(
+                pd.read_csv(os.path.join(self.metadata_directory, 'MAMe_labels.csv'), names=["description"],
+                            index_col=0).iterrows())
         }
 
     def get_train_loader(self) -> DataLoader:
         return DataLoader(
-            dataset=MAMeDataset(metadata_path=os.path.join(self.directory, 'MAMe_dataset.csv'),
-                                key=self.label_descriptions, root_dir=self.directory, split='train', transform=None),
+            dataset=MAMeDataset(metadata_path=os.path.join(self.metadata_directory, self.metadata_file),
+                                key=self.label_descriptions, root_dir=self.images_directory, split='train',
+                                transform=transforms.ToTensor()),
             shuffle=True, batch_size=self.batch_size
         )
 
     def get_test_loader(self) -> DataLoader:
         return DataLoader(
-            dataset=MAMeDataset(metadata_path=os.path.join(self.directory, 'MAMe_dataset.csv'),
-                                key=self.label_descriptions, root_dir=self.directory, split='test', transform=None),
+            dataset=MAMeDataset(metadata_path=os.path.join(self.metadata_directory, self.metadata_file),
+                                key=self.label_descriptions, root_dir=self.images_directory, split='test',
+                                transform=transforms.ToTensor()),
             shuffle=True, batch_size=self.batch_size
         )
 
     def get_val_loader(self) -> DataLoader:
         return DataLoader(
-            dataset=MAMeDataset(metadata_path=os.path.join(self.directory, 'MAMe_dataset.csv'),
-                                key=self.label_descriptions, root_dir=self.directory, split='val', transform=None),
+            dataset=MAMeDataset(metadata_path=os.path.join(self.metadata_directory, self.metadata_file),
+                                key=self.label_descriptions, root_dir=self.images_directory, split='val',
+                                transform=transforms.ToTensor()),
             shuffle=True, batch_size=self.batch_size
         )
 
@@ -61,4 +76,4 @@ class MAMe(Data):
         pass
 
     def get_data_shape(self):
-        return [3, 256, 256]
+        return [[3, 256, 256], 29]
