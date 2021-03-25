a4b7c1c8593b64a745f66d11e4b154d96b7f4b64
adriantormos <adriantormos98@gmail.com>
2021-03-25 18:27:02 +0100

    Add OdinP9 architecture
    This model uses the original input and an additional resized 32x32 version of it.

--- code/auxiliary_files/model_methods/nets.py
+++ code/auxiliary_files/model_methods/nets.py
@@ -1,6 +1,7 @@
 import sys
 import torch
 import torch.nn as nn
+from torchvision.transforms import Resize
 
 
 def select_net(config, data_shape):
@@ -1398,3 +1399,81 @@ class OdInception9(nn.Module):
         x = torch.cat((inception2a, inception2b, inception2c), dim=1)
         x = self.cnn_2(x)
         return self.main_fcc(x)
+
+
+class OdinP9(nn.Module):
+    def __init__(self, config, data_shape):
+        super(OdinP9, self).__init__()
+        number_bands = data_shape[0]
+        filter_scale = config['filter_scale']
+        output_size = config['output_size']
+        self.main_cnn = nn.Sequential(
+            # input is (number_bands) x 256 x 256
+            nn.Conv2d(number_bands, filter_scale, kernel_size=(3, 3), stride=(1, 1), padding=(1,1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            # state size (filter_scale) x 128 x 128
+            nn.Conv2d(filter_scale, filter_scale*2, kernel_size=(3, 3), stride=(1, 1), padding=(1,1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            nn.BatchNorm2d(filter_scale*2),
+            # state size (filter_scale*2) x 64 x 64
+            nn.Conv2d(filter_scale*2, filter_scale*4, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            # state size (filter_scale*4) x 32 x 32
+            nn.Conv2d(filter_scale*4, filter_scale*8, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            nn.BatchNorm2d(filter_scale * 8),
+            # state size (filter_scale*8) x 16 x 16
+            nn.Conv2d(filter_scale * 8, filter_scale * 16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            # state size (filter_scale*16) x 8 x 8
+            nn.Conv2d(filter_scale * 16, filter_scale * 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            # state size (filter_scale*32) x 4 x 4
+            nn.Flatten()
+            # state size filter_scale * 32 * 4 * 4
+        )
+        self.small_cnn = nn.Sequential(
+            # input is (number_bands) x 32 x 32
+            nn.Conv2d(number_bands, filter_scale, kernel_size=(3, 3), stride=(1, 1), padding=(1,1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            # state size (filter_scale) x 16 x 16
+            nn.Conv2d(filter_scale, filter_scale*2, kernel_size=(3, 3), stride=(1, 1), padding=(1,1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            # state size (filter_scale) x 8 x 8
+            nn.Conv2d(filter_scale*2, filter_scale*4, kernel_size=(3, 3), stride=(1, 1), padding=(1,1)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            # state size filter_scale * 4 * 4 * 4
+            nn.Flatten()
+        )
+        self.main_fcc = nn.Sequential(
+            # input is filter_scale * 32 * 4 * 4
+            nn.Linear(filter_scale * 32 * 4 * 4 + filter_scale * 4 * 4 * 4, filter_scale * 8 * 2 * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm1d(filter_scale * 8 * 2 * 2),
+            # state size filter_scale * 8 * 2 * 2
+            nn.Linear(filter_scale * 8 * 2 * 2, filter_scale * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size filter_scale * 4
+            nn.Linear(filter_scale * 4, output_size),
+            # state size output_size
+            nn.Sigmoid()
+        )
+        print(self)
+
+    def forward(self, x):
+        x_main = self.main_cnn(x)
+        resized_x = Resize((32, 32))(x)
+        x_small = self.small_cnn(resized_x)
+
+        final_x = torch.cat((x_main, x_small), dim=1)
+        return self.main_fcc(final_x)
+
